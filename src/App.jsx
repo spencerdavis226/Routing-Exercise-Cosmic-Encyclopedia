7cb597a601f60808b38b2f5698b4b2f9dfe96c2c
Spencer Davis <spencerdavis226@gmail.com>
2025-02-18 12:33:21 -0500

    Added HomePage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 
 // PAGES
+import HomePage from './pages/HomePage';
+import ContentPage from './pages/ContentPage';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/planets" element={<ContentPage category="planets" />} />
-        <Route path="/stars" element={<ContentPage category="stars" />} />
-        <Route path="/nebulae" element={<ContentPage category="nebulae" />} />
-        <Route path="/galaxies" element={<ContentPage category="galaxies" />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/planets" element={<ContentPage category="planets" />} />
+          <Route path="/stars" element={<ContentPage category="stars" />} />
+          <Route path="/nebulae" element={<ContentPage category="nebulae" />} />
+          <Route
+            path="/galaxies"
+            element={<ContentPage category="galaxies" />}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Cosmic Encylopedia</h1>
+      <p>Explore the wonders of the universe by selecting a category above!</p>
+    </div>
+  );
+}
